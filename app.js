0759bd63defdb06d795a1b078bd4f3339f111332
Rachel Rosensweig <rachel.rosensweig@generalassemb.ly>
2014-07-27 19:24:08 +0000

    Added my name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "YOUR_NAME_HERE";
+	var myName = "Rachel";
 	app.get("/", function (req, resp) {
 		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
 		resp.end();
