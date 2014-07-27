a7cb375a3e697bdb0bdeccee28f2254985b4ba43
Rachel Rosensweig <rachel.rosensweig@generalassemb.ly>
2014-07-27 22:03:41 +0000

    made my site

--- app.js
+++ app.js
@@ -7,8 +7,36 @@ appHelper.runApp(function(app, db) {
 
 	var myName = "Rachel";
 	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
+    var obj = {
+      "name": myName,
+      "food": "Pizza:",
+      }
+    resp.render("index", obj );
 	});
 
-})
+  app.get("/icecream/:flavor", function(req, resp) {
+    resp.render("icecream", { flavor: req.params.flavor });
+  });
+  
+  app.get("/travel", function(ref, resp) {
+   db.findArray({}, function(results) {
+     var params = {
+       "places": results 
+     }
+       resp.render("travel", params);
+   })
+  });
+     app.get("/travel/rating/:rating", function(ref, resp) {
+       var rating = parseInt(req.params.rating);
+       db.findArray({ rating: rating }, function(results) {
+     var params = {
+       "places": results 
+     }
+       resp.render("travel", params);
+     })
+  });
+   
+  
+
+               
+})  
